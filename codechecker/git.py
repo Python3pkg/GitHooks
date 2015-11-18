34ba961011f820dbdb9e90f3e0254a14f16002c5
Sławek Dróżdż <slawek.drozdz@gmail.com>
2015-11-18 15:25:09 +0100

    Change function that gets staged files
    Now absolute paths of staged files are returned

--- codechecker/git.py
+++ codechecker/git.py
@@ -35,12 +35,16 @@ def find_repository_dir(curdir):
 
 def get_staged_files():
     """Return files in git staging area"""
+    def normpath(file_relpath):
+        """Get absolute path"""
+        return path.join(repository_path, file_relpath)
+    repository_path = find_repository_dir(os.getcwd())
     git_args = 'git diff --staged --name-only'.split()
     git_process = Popen(git_args, stdout=PIPE)
     git_process.wait()
 
-    # Filter deleted files
-    file_list = [f for f in [f.strip().decode(sys.stdout.encoding)
+    # read staged files and filter deleted ones
+    file_list = [f for f in [normpath(f.decode(sys.stdout.encoding).strip())
                              for f in git_process.stdout.readlines()]
                  if os.path.exists(f)]
     return file_list
